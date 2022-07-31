a0dad25346d117d88efcefb1158262a8ae319804
SandroAma <sandroama123@gmail.com>
2022-07-31 18:40:40 -0400

    random gen

--- ES6/Debut/bmi.js
+++ ES6/Debut/bmi.js
@@ -1,1 +1,9 @@
 // BMI Calculator
+
+// BMI = weight(kg) / height ^2 (m^2);
+function BMI(weight, height){
+    return Math.round(weight / Math.pow(height,2));
+}
+
+var myBMI = BMI(98,1.84);
+console.log(myBMI);
---
+++ ES6/Debut/rand.js
@@ -0,0 +1,11 @@
+var n = Math.random();
+Math.floor(n*6) +1;
+
+
+// scoring
+prompt("what is your name?")
+prompt("what is the destination?");
+
+var destScore = Math.random() * 100;
+destScore = Math.floor(destScore) +1;
+alert("Your dest score is "+ destScore + "%")

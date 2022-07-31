1f63679fff98afaa2f15793d0f60619fe7678954
SandroAma <sandroama123@gmail.com>
2022-07-31 15:06:30 -0400

    update misc.js

--- ES6/Debut/misc.js
+++ ES6/Debut/misc.js
@@ -3,3 +3,9 @@ var name ="Sandro";
 
 // alert(message + " there,"+ " " + "Sandro");
 
+// Numbers
+var e = 9 % 6;
+
+var dogAge = prompt("How old is your dog");
+var humanAge = (dogAge-2)*4 +21;
+alert("Your dog in human years is: " + humanAge + " years old");

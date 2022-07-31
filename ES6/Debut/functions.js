9babb56ded0d9b10125e4997e261a703b7bebcea
SandroAma <sandroama123@gmail.com>
2022-07-31 15:51:11 -0400

    practice

--- ES6/Debut/functions.js
+++ ES6/Debut/functions.js
@@ -18,3 +18,21 @@ function main(){
        move(); }
  }
  
+//
+function lifeInWeeks(age){
+    var yearsRemaining = 90 -age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining *12;
+    console.log("You have " + days + " days, " + weeks + "weeks, and " + months + " months left.");
+}
+
+function calcBottles(startingMoney, costPerBottle){
+    var numberOfBottles = Math.floor(startingMoney/ costPerBottle);
+    return numberOfBottles;
+}
+
+function calcChange(startingMoney, costPerBottle){
+    var change = startingMoney % costPerBottle;
+    return change;
+}

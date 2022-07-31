23e866a815929bb84ac9cf164bcd99d37e8bfc56
SandroAma <sandroama123@gmail.com>
2022-07-31 13:40:37 -0400

    debut in js

---
+++ ES6/Debut/vars.js
@@ -0,0 +1,31 @@
+// commands
+alert("Hello");
+prompt("What is your name");
+
+// Numbers
+1, 2, 3
+typeof(5); // number
+
+// Boolean
+true
+false
+typeof(true);
+
+// strings
+"Sandro"
+typeof("Sandro");
+
+// Vars
+var myName = "Sandro";
+myName = "Anna";
+
+// prompt("What is your name");
+var myName = "Sandro";
+alert(myName + " is giving the orders now!");
+
+// using prompt
+// prompt("What is your name");
+var myName = "Sandro";
+alert(myName + " is giving the orders now!");
+var yourName = prompt("What is your name?")
+alert(yourName + " is giving the co-orders now!");

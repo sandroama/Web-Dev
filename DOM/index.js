2fe529d4acb3c5bf11ddf49576ce499a0762272b
SandroAma <sandroama123@gmail.com>
2022-08-01 12:09:04 -0400

    update dom commands

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,6 @@
 document.querySelector("h1").innerHTML = "Good Bye";
+
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "Hello there"; // manipulating on html elements
+heading.style.color = "red"; // editing css
+document.querySelector("input").click() // simulating click

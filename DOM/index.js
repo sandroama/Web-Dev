f9c99aede2cb570d919d6c9fe3a3137b15250491
SandroAma <sandroama123@gmail.com>
2022-08-02 10:12:43 -0400

    update index.js

--- DOM/index.js
+++ DOM/index.js
@@ -13,14 +13,31 @@
 // changeCol.style.color="purple";
 
 
-// Query Selector
-document.querySelector("li");
-document.querySelector("#title");
-document.querySelector("li.item");
-document.querySelector("#list a");
-document.querySelector("#list .item");
-document.querySelectorAll("#list .item"); // array of elements
-document.querySelectorAll("#list .item")[index]; // specific element
+// // Query Selector
+// document.querySelector("li");
+// document.querySelector("#title");
+// document.querySelector("li.item");
+// document.querySelector("#list a");
+// document.querySelector("#list .item");
+// document.querySelectorAll("#list .item"); // array of elements
+// document.querySelectorAll("#list .item")[index]; // specific element
 
-// challenge
-document.querySelector("#list a").style.color="yellow"
+// // challenge
+// document.querySelector("#list a").style.color="yellow";
+
+// document.querySelector("h1").style.fontSize = "10rem";
+
+
+// // challenge
+
+// document.querySelector(".btn").style.backgroundColor="yellow";
+
+
+// // class list
+
+// document.querySelector("button").classList; // array of classes on a button
+// document.querySelector("button").classList.add("invisible");
+
+
+// document.querySelector(".btn").classList.add("invisible");
+// document.querySelector(".btn").classList.remove("invisible");

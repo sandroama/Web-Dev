89b61fb712879964e6bdcae2f5475b82b753359e
SandroAma <sandroama123@gmail.com>
2022-08-01 22:21:11 -0400

    update index.js

--- DOM/index.js
+++ DOM/index.js
@@ -1,6 +1,13 @@
-document.querySelector("h1").innerHTML = "Good Bye";
+// document.querySelector("h1").innerHTML = "Good Bye";
 
-var heading = document.firstElementChild.lastElementChild.firstElementChild;
-heading.innerHTML = "Hello there"; // manipulating on html elements
-heading.style.color = "red"; // editing css
-document.querySelector("input").click(); // simulating click
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+// heading.innerHTML = "Hello there"; // manipulating on html elements
+// heading.style.color = "red"; // editing css
+// document.querySelector("input").click(); // simulating click
+
+
+// var challenge = document.firstElementChild.lastElementChild.lastElementChild.lastElementChild;
+// challenge.innerHTML = "sandro";
+
+// var changeCol = document.getElementsByTagName("li")[2].style.color;
+// changeCol.style.color="purple";

8c4e3070eccdb50da06930a91186e15217687ed0
SandroAma <sandroama123@gmail.com>
2022-08-01 21:28:47 -0400

    update every_file

--- DOM/index.js
+++ DOM/index.js
@@ -3,4 +3,4 @@ document.querySelector("h1").innerHTML = "Good Bye";
 var heading = document.firstElementChild.lastElementChild.firstElementChild;
 heading.innerHTML = "Hello there"; // manipulating on html elements
 heading.style.color = "red"; // editing css
-document.querySelector("input").click() // simulating click
+document.querySelector("input").click(); // simulating click

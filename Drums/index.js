313faf0b517f91f909f866eb0e7f856c00ec3dff
SandroAma <sandroama123@gmail.com>
2022-08-05 22:44:44 -0400

    comments

--- Drums/index.js
+++ Drums/index.js
@@ -1,27 +1,27 @@
-// // btn click handler
-// function handleClick(){
-//     alert("I got clicked");
+// btn click handler
+function handleClick(){
+    alert("I got clicked");
+}
+
+for(let i = 0; i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+        alert("I got clicked!");
+    });
+}
+
+
+// // High order function ex
+// function add(num1, num2){
+//     return num1 + num2;
 // }
 
-// for(let i = 0; i<document.querySelectorAll(".drum").length;i++){
-//     document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-//         alert("I got clicked!");
-//     });
+// function multiply(num1,num2){
+//     return num1 * num2;
 // }
 
+// function calc(num1, num2, operator){
+//     return operator(num1,num2);
+// }
 
-// High order function ex
-function add(num1, num2){
-    return num1 + num2;
-}
-
-function multiply(num1,num2){
-    return num1 * num2;
-}
-
-function calc(num1, num2, operator){
-    return operator(num1,num2);
-}
-
-console.log(calc(4,5,add));
-console.log(calc(4,5,multiply));
+// console.log(calc(4,5,add));
+// console.log(calc(4,5,multiply));

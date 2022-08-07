4dc2fef9cab98b2436b4f257a9f9115e7e3003a4
SandroAma <sandroama123@gmail.com>
2022-08-06 23:39:10 -0400

    Learning jQuery

---
+++ jQuery/index.js
@@ -0,0 +1,24 @@
+
+// $(document).ready(function(){
+//     $("h1").css("color","red");
+// });
+
+// $("h1").css("color","red");
+
+// document.querySelector("h1");
+// $("h1");
+// $("button");
+
+// $("h1").css("color","blue");
+// console.log($("h1").css("font-size")); // inputing one value is getting the attribute value
+// $("h1").css("font-size","5rem"); // inputing 2 values is  setting an attribute to that second value
+
+$("h1").addClass("big-title margin50");
+// $("h1").removeClass("big-title");
+
+$("h1").text("bye");
+$("button").html("<em>This is me</em>");
+
+$("a").attr("href","https://www.yahoo.com");
+
+// $("h1").attr("class")

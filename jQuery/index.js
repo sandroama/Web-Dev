d62445be4549a0ee26e344a25b383b2916f5e2fa
SandroAma <sandroama123@gmail.com>
2022-08-07 19:20:13 -0400

    update index.js

--- jQuery/index.js
+++ jQuery/index.js
@@ -28,12 +28,52 @@
 //     $("h1").css("color","blue");
 // });
 
-for(let i=0;i<5;i++){
-    document.querySelectorAll("button")[i].addEventListener("click",function(){
-        document.querySelector("h1").style.color = "blue";
-    });
-}
 
+
+
+
+// --- Onclick events ---
+// for(let i=0;i<5;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         document.querySelector("h1").style.color = "blue";
+//     });
+// }
+
+// jQuery on click events
 $("button").click(function(){
     $("h1").css("color","purple");
 });
+
+$("input").keypress(function(event){
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover",function(){
+    $("h1").css("color","cyan");
+});
+
+
+// // --- Adding HTML elements before and after --------------------------
+// $(h1).before("<button>New</button>");
+// $(h1).after("<button>New</button>");
+
+// $(h1).prepend("<button>New</button>"); // prepend adds before inner html content 
+// $(h1).append("<button>New</button>"); // prepend adds after inner html content
+
+
+// --- Hide and show html elements --------------------------------
+
+$("button").on("click",function(){
+    // $("h1").fadeToggle(); // Fade out the element
+    // $("h1").slideToggle(); // Slide out the element
+    
+    $("h1").animate({opacity: 0.5, margin: "20"});
+    // https://www.w3schools.com/jquery/jquery_ref_effects.asp
+
+    // $("h1").toggle();
+    // $("h1").hide();
+});
+
+// $(h1).hide();
+// $(h1).show();
+ 

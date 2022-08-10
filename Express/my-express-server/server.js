3968539658c6bd2858ccb4378fe28808487ef82b
SandroAma <sandroama123@gmail.com>
2022-08-10 08:26:44 -0400

    update express.js

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+
+
+// What should happen when someone makes get request at home root 
+// Callback Function
+app.get("/",function(request,response){ 
+    response.send("<h1>Hello, world!</h1>");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });

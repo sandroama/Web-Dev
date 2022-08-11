8c5d45a966828869fe5b85dae0c2b49b48443ca6
SandroAma <sandroama123@gmail.com>
2022-08-11 00:01:48 -0400

    express js and calculator

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -8,6 +8,18 @@ app.get("/",function(request,response){
     response.send("<h1>Hello, world!</h1>");
 });
 
+app.get("/contact",function(req,res){
+    res.send("Contact me at: sa@gmail.com");
+});
+
+app.get("/about",function(req,res){
+    res.send("Contact me at: sa@gmail.com");
+});
+
+app.get("/hobies",function(req,res){
+    res.send("<h1>My Hob</h1>");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });

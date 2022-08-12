a2420950aa28b224860b8191149e454df04cee36
SandroAma <sandroama123@gmail.com>
2022-08-11 23:55:56 -0400

    commit

--- APIs/index.js
+++ APIs/index.js
@@ -1,2 +1,4 @@
 // Paths -> https://v2.jokeapi.dev/joke/Programming?blacklistFlags=political&contains=debugging
-// 705bb38bd85b460dc33273529a37e174
+// 705bb38bd85b460dc33273529a37e174 
+//https://sv443.net/jokeapi/v2/#try-it
+// https://api.kanye.rest/#

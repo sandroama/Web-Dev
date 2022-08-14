10904f061a87807e79cb0c8d9a806c6d3c3a2223
SandroAma <sandroama123@gmail.com>
2022-08-14 00:55:59 -0400

    Beautified

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -4,31 +4,31 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true})); // necessary code to start using body parser
+app.use(bodyParser.urlencoded({ extended: true })); // necessary code to start using body parser
 
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname +"/index.html");
-    
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+
 });
 
-app.post("/",function(req,res){
+app.post("/", function (req, res) {
     const city = req.body.cityName;
-    const apiKey="705bb38bd85b460dc33273529a37e174"
+    const apiKey = "705bb38bd85b460dc33273529a37e174"
     const units = "metric"
-    const url = "https://api.openweathermap.org/data/2.5/weather?APPID="+apiKey+"&units="+units+"&q="+city;
+    const url = "https://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&units=" + units + "&q=" + city;
 
-    https.get(url,function(response){
+    https.get(url, function (response) {
         console.log(response.statusCode);
-        response.on("data",function(data){
+        response.on("data", function (data) {
             const weatherData = JSON.parse(data);
-            const temp =  weatherData.main.temp;
+            const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
-            const imageURL = "https://openweathermap.org/img/wn/" + icon +"@2x.png"
-            
-            res.write("<p>The weather is currently "+weatherDescription + "</p>");
-            res.write("<h1>The temperature in " + city+ " is "+temp + " degrees Celcius</h1>");
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + city + " is " + temp + " degrees Celcius</h1>");
             res.write("<img src=" + imageURL + ">")
             res.send();
             // console.log(temp);
@@ -53,6 +53,6 @@ app.post("/",function(req,res){
 
 
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000.")
 })

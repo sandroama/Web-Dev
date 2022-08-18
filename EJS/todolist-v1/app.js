6399a9c0f2389f3ab5b2ddd305d896c7f192ef6a
SandroAma <sandroama123@gmail.com>
2022-08-18 14:06:05 -0400

    work update

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -4,7 +4,8 @@ const ejs = require("ejs");
 
 const app = express();
 
-var items = [];
+let items = [];
+let workItems= [];
 
 app.set("view engine", "ejs"); // Using eJS with express
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,15 +14,15 @@ app.use(express.static("public"))
 
 app.get("/", function (req, res) {
      
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long",
         year: "numeric"
     };
      
-    var day = today.toLocaleDateString("en-us",options);
+    let day = today.toLocaleDateString("en-us",options);
      
     // var day = new Date().toLocaleDateString('en-us', {
     //     weekday: "long",
@@ -30,16 +31,35 @@ app.get("/", function (req, res) {
     //     day:"numeric"
     // });
           
-    res.render("list",{type_day: day,newListItems: items});
+    res.render("list",{listTitle: day,newListItems: items});
     
      
 });  
      
+
+
+
 app.post("/",function(req,res){
-    var item = req.body.newItem;
+    console.log(req.body);
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 });
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List",newListItems: workItems});
+
+
+});
+
+app.post("/work",function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+
+})
+
+
 
 
 app.listen(3000, function () {

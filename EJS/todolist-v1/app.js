269b8234fda3254df04de93e710333dd809a6737
SandroAma <sandroama123@gmail.com>
2022-08-17 22:00:21 -0400

    ejs update

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -1,20 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
 
 const app = express();
 
-app.get("/",function(req,res){
+app.set("view engine", "ejs"); // Using eJS with express
+
+app.get("/", function (req, res) {
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-    if(currentDay ===6 || currentDay===0){
-        res.write("<h1>Yay it's the weekend!</h1>");
-    }else{
-        res.sendFile(__dirname+"/index.html");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 5:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to " + currentDay);
     }
+
+    res.render("list", {
+        type_day: day
+    });
 
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });

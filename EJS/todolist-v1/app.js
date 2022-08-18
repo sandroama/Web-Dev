3496d6781e3679dda2b9517f4a5e70b9e4510bdd
SandroAma <sandroama123@gmail.com>
2022-08-17 23:31:28 -0400

    delete switch case

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -9,34 +9,15 @@ app.set("view engine", "ejs"); // Using eJS with express
 
 app.get("/", function (req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US",options);
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 5:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to " + currentDay);
-    }
+   
 
     res.render("list", {
         type_day: day

22f5864f2cf8659b88b75c120828a2e11e5883dc
SandroAma <sandroama123@gmail.com>
2022-08-25 23:48:01 -0400

    just mongoose

--- MongoDB/FruitsProject/app.js
+++ MongoDB/FruitsProject/app.js
@@ -1,67 +1,100 @@
-// const MongoClient = require("mongodb").MongoClient;
-// const assert = require("assert");
-//-----
+
 const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
-// Replace the uri string with your connection string.
-const url =
-  "mongodb://localhost:27017";
-
-const dbName = "fruitsDB";
-
-const client = new MongoClient(url,{useNewUrlParser:true});
-
-client.connect(function(err){
-    assert.equal(null,err);
-    console.log("Connect succesfully to server");
-    const db = client.db(dbName);
-    findDocuments(db,function(){
-        client.close();
-    })
+// New Schema
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
 });
 
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-      {
-        name:"Apple",
-        score:9,
-        review:"Essential"
-      },
-      {
-        name:"Banana",
-        score:8,
-        review:"Good fruit"
-      },
-      {
-        name:"Plum",
-        score:7,
-        review:"Helpful"
-      }
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.insertedCount); // https://mongodb.github.io/node-mongodb-native/4.0/classes/collection.html#insertmany
-      assert.equal(3, Object.keys(result.insertedIds).length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-  }
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 8,
+  review: "Good and essential"
+});
+
+fruit.save();
 
 
-  const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-      assert.equal(err, null);
-      console.log("Found the following records");
-      console.log(fruits)
-      callback(fruits);
-    });
-  }
 
-// mongo --version 
-// MongoClient.connect("mongodb://localhost:27017/fruits",{useNewUrlParser:true})
+
+
+
+
+
+
+
+
+
+
+// // MONGODB AND NODE JS TOGETHER
+
+// // const MongoClient = require("mongodb").MongoClient;
+// // const assert = require("assert");
+// //-----
+// // Replace the uri string with your connection string.
+// const url =
+//   "mongodb://localhost:27017";
+
+// const dbName = "fruitsDB";
+
+// const client = new MongoClient(url, { useNewUrlParser: true });
+
+// client.connect(function (err) {
+//   assert.equal(null, err);
+//   console.log("Connect succesfully to server");
+//   const db = client.db(dbName);
+//   findDocuments(db, function () {
+//     client.close();
+//   })
+// });
+
+
+// const insertDocuments = function (db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection('fruits');
+//   // Insert some documents
+//   collection.insertMany([
+//     {
+//       name: "Apple",
+//       score: 9,
+//       review: "Essential"
+//     },
+//     {
+//       name: "Banana",
+//       score: 8,
+//       review: "Good fruit"
+//     },
+//     {
+//       name: "Plum",
+//       score: 7,
+//       review: "Helpful"
+//     }
+//   ], function (err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.insertedCount); // https://mongodb.github.io/node-mongodb-native/4.0/classes/collection.html#insertmany
+//     assert.equal(3, Object.keys(result.insertedIds).length);
+//     console.log("Inserted 3 documents into the collection");
+//     callback(result);
+//   });
+// }
+
+
+// const findDocuments = function (db, callback) {
+//   // Get the documents collection
+//   const collection = db.collection('fruits');
+//   // Find some documents
+//   collection.find({}).toArray(function (err, docs) {
+//     assert.equal(err, null);
+//     console.log("Found the following records");
+//     console.log(fruits)
+//     callback(fruits);
+//   });
+// }
+
+// // mongo --version 
+// // MongoClient.connect("mongodb://localhost:27017/fruits",{useNewUrlParser:true})

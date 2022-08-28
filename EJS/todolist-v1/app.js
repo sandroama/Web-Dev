3a387691ebb3f9592101e8bbbb9a848a634822f7
SandroAma <sandroama123@gmail.com>
2022-08-28 11:31:42 -0400

    Update app.js

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin:<password>@cluster0.zwivaax.mongodb.net/?retryWrites=true&w=majority/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin:Sandroama123@cluster0.zwivaax.mongodb.net/todolistDB", { useNewUrlParser: true });
 
 const itemsSchema = {
   name: String

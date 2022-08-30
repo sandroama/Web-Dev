0bea64b9711eb0810f65827bda1fc25c00f82bd1
SandroAma <sandroama123@gmail.com>
2022-08-29 21:32:32 -0400

    Update app.js

--- Rest/Wiki-API/app.js
+++ Rest/Wiki-API/app.js
@@ -11,7 +11,7 @@ app.set("view engine","ejs");
 app.use(bodyParser,urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB",{userNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/wikiDB",{userNewUrlParser: true});
 
 const articleSchema ={
     title: String,
@@ -21,10 +21,18 @@ const articleSchema ={
 const Article = mongoose.model("Article",articleSchema);
 
 
-
-
-
 //---
+
+app.get("/articles",function(req,res){
+    Article.find(function(err,foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else{
+            res.send(err);
+        }
+    });
+
+})
 
 app.listen(3000,function(){
     console.log("Server running on port 3000")
